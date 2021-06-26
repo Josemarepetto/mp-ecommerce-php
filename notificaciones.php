ba3943061207207bbd2dce64c479190d79ceb9a5
JOSEMAREPETTO889 <josemarepetto@gmail.com>
2021-06-26 19:32:32 -0300

    Mensaje

--- notificaciones.php
+++ notificaciones.php
@@ -3,17 +3,16 @@ require './vendor/autoload.php';
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
     echo "<h2>Notificaciones</h2><br>";
 
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $raw_payload = file_get_contents('php://input');
         $payload = json_decode($raw_payload, true);
         echo $payload;
-    }
+
 
     // $json = file_get_contents('php://input');
     // $data = json_decode($json,true);
     // echo $data;
 
-    
+
     // if($data->type!=null){
     //     $type = $json_data->type;
     //     switch($type) {
