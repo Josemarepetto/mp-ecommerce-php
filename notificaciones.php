89e9a44ad186e3c202d59421bfd738b9a9af8611
Josemarepetto <48872679+Josemarepetto@users.noreply.github.com>
2021-06-26 20:29:07 -0300

    2

--- notificaciones.php
+++ notificaciones.php
@@ -2,36 +2,25 @@
 require './vendor/autoload.php';
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
     echo "<h2>Notificaciones</h2><br>";
+    $data = file_get_contents('php://input');
+    $json_data = json_decode($data);
 
-        
-
-        	$data = file_get_contents("php://input");
-        	$decode = json_decode($data);
-        echo echo "<script>console.log('Debug Objects: " . $decode . "' );</script>";
-
-
-    // $json = file_get_contents('php://input');
-    // $data = json_decode($json,true);
-    // echo $data;
-
-
-    // if($data->type!=null){
-    //     $type = $json_data->type;
-    //     switch($type) {
-    //     case "payment":
-    //         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-    //         echo $payment;
-    //         break;
-    //     case "plan":
-    //         $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-    //         break;
-    //     case "subscription":
-    //         $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-    //         break;
-    //     case "invoice":
-    //         $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-    //         break;
-    // }
-    // }
+        $type = $json_data->type;
+        switch($type) {
+        case "payment":
+            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            echo $payment;
+            break;
+        case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+    }
+    
     
 ?>
