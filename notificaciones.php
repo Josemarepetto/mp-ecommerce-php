2b488bf276e9f8f584455eeaf8fe63951f721337
Josemarepetto <48872679+Josemarepetto@users.noreply.github.com>
2021-06-26 20:22:43 -0300

    1

--- notificaciones.php
+++ notificaciones.php
@@ -3,11 +3,11 @@ require './vendor/autoload.php';
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
     echo "<h2>Notificaciones</h2><br>";
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
 
         	$data = file_get_contents("php://input");
         	$decode = json_decode($data);
-        echo echo "<script>console.log('Debug Objects: " . $decode . "' );</script>";}
+        echo echo "<script>console.log('Debug Objects: " . $decode . "' );</script>";
 
 
     // $json = file_get_contents('php://input');
