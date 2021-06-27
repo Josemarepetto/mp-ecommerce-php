566d966507f1cf8de0e690ebfcb2b66363469c03
Josemarepetto <48872679+Josemarepetto@users.noreply.github.com>
2021-06-26 21:00:43 -0300

    1

--- notificaciones.php
+++ notificaciones.php
@@ -1,26 +1,22 @@
 <?php
-require './vendor/autoload.php';
-MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-    echo "<h2>Notificaciones</h2><br>";
-    $data = file_get_contents('php://input');
-    $json_data = json_decode($data);
 
-        $type = $json_data->type;
-        switch($type) {
-        case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            echo $payment;
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            break;
+    require './vendor/autoload.php';
+    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+    class webhook{
+
+      public function __construct(){
+        $json= file_get_contents('php://input');
+        $decoded = json_decode($json, true);
+
+        //Salvo el Body del Json
+        ob_start();
+        var_dump($decoded);
+        $input = ob_get_contents();
+        ob_end_clean();
+
+        //Grabo en archivo
+        file_put_contents('inputs.log',$input . PHP_EOL, FILE_APPEND);
+      }
     }
-    
-    
+    new webhook();
 ?>
